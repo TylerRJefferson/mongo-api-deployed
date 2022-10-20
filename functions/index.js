1a21728a5f0e4ab0be79d3d6aaeb6893aca7ae02
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-20 15:40:11 -0400

    added add new furniture function

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,13 @@
 import functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
-import { getAllFurniture } from "./src/furniture.js";
+import { getAllFurniture, addNewFurniture } from "./src/furniture.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/furniture", (getAllFurniture));
+app.post("/furniture", (addNewFurniture));
 
 export const api = functions.https.onRequest(app);
--- functions/src/furniture.js
+++ functions/src/furniture.js
@@ -13,3 +13,20 @@ export async function getAllFurniture(req, res) {
   // send back the array of furniture
   res.send(collection);
 }
+
+export async function addNewFurniture(req, res){
+  // get new furniture from the body of the request
+  const { brand, model, type, price } = req.body
+  const newFurniture = { brand, model, type, price: Number(price) }
+  // connect to db
+  const db = dbConnect()
+  // put this new furniture into our furniture collection in db
+  await db.collection("furniture").insertOne(newFurniture)
+  // catch any errors -> status 500
+    .catch(err => {
+      res.status(500).send(err)
+      return
+    })
+  // return a response -> status 201
+  res.status(201).send({ message: "Furniture added"})
+}
